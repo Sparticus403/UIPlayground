a3d63071ca90277a0ecac56e4b8959ca530c1d84
Sparticus403 <twil8980@740s-pl-332.local>
2016-09-21 08:23:17 -0600

    Testing UI components for playgrounds

---
+++ Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var DynamicView = UIView(frame: CGRectMake(100, 200, 100, 100))
+DynamicView.backgroundColor=UIColor.greenColor()
+DynamicView.layer.cornerRadius=100
+DynamicView.layer.borderWidth=2
+DynamicView
+
